3612ab8377b05e3e4309cde7c9723a16578bbc20
lorenzo-damico <loredam@hotmail.it>
2020-09-02 17:12:08 +0200

    Aggiunto scorrimento pallini.

--- js/script.js
+++ js/script.js
@@ -12,14 +12,17 @@
 $(document).ready(function() {
 
   // 1. Imposto un evento click sulla freccia destra per scorrere in avanti.
+  //    Ripeto le stesse operazioni anche sul pallino colorato.
   $(".next").click(
     function() {
 
       // 2. Per prima cosa definisco l'immagine che possiede la classe active attualmente.
       var imgActive = $("img.active");
+      var pallinoActive = $(".nav i.active")
 
       // 3. Rimuovo la classe dall'immagine affinchè scompaia.
       imgActive.removeClass("active");
+      pallinoActive.removeClass("active");
 
       // 4. Definisco l'immagine successiva.
       //    Se è l'ultima immagine, ovvero ha classe last, reimposto la prima immagine.
@@ -30,8 +33,15 @@ $(document).ready(function() {
         var nextImg = imgActive.next();
       }
 
+      if (pallinoActive.hasClass("last") == true) {
+        var nextPallino = $(".nav i.first");
+      } else {
+        var nextPallino = pallinoActive.next();
+      }
+
       // 5. Aggiungo la classe active all'immagine successiva.
       nextImg.addClass("active");
+      nextPallino.addClass("active");
     }
   );
 
@@ -41,9 +51,11 @@ $(document).ready(function() {
 
       // 2. Per prima cosa definisco l'immagine che possiede la classe active attualmente.
       var imgActive = $("img.active");
+      var pallinoActive = $(".nav i.active");
 
       // 3. Rimuovo la classe dall'immagine affinchè scompaia.
       imgActive.removeClass("active");
+      pallinoActive.removeClass("active");
 
       // 4. Definisco l'immagine pecedente.
       //    Se è la prima immagine, ovvero ha classe first, reimposto l'ultima immagine.
@@ -54,8 +66,15 @@ $(document).ready(function() {
         var nextImg = imgActive.prev();
       }
 
+      if (pallinoActive.hasClass("first") == true) {
+        var nextPallino = $(".nav i.last");
+      } else {
+        var nextPallino = pallinoActive.prev();
+      }
+
       // 5. Aggiungo la classe active all'immagine precedente.
       nextImg.addClass("active");
+      nextPallino.addClass("active");
     }
   );
 
